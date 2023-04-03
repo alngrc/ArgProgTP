3839e64d2f98ba54ac2bbc6bf90ea0ab38fdfe31
alngr_ <alan.garciag10@outlook.com>
2023-04-03 00:47:11 -0300

    Se agrego ABOUT, h1, h2 y animaciones con JS y CSS

---
+++ main.js
@@ -0,0 +1,13 @@
+const observ = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('mostrar');
+        } else {
+            entry.target.classList.remove('mostrar');
+        }
+    });
+});
+
+const elementosOcultos = document.querySelectorAll('.oculto');
+elementosOcultos.forEach((el) => observ.observe(el));
